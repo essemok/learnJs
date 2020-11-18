c30670923f16b86f0c34135fce09b86efaa98f31
Semyon Sporysh <beee.semok@gmail.com>
2020-11-18 14:55:50 +0300

    First task work in browser

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 const number = prompt('Сколько фильмов вы уже посмотрели?', '0');
 
 console.log(number);
+
+
+const personalMovieDb = {
+  count: number,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+}
+
+console.log(personalMovieDb)
+
+const firstMovie = prompt('Один из последних просмотренных фильмов?', '');
+const firstRating = prompt('На сколько оцените его?', '');
+const secondMovie = prompt('Один из последних просмотренных фильмов?', '');
+const secondRating = prompt('Насколько оцените его?', '');
+
+let movies = {};
+
+movies[firstMovie] = firstRating;
+movies[secondMovie] = secondRating;
+
+console.log(movies);
