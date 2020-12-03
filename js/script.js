fbe0fce76c2307ce8b22c4985f7220bb3bfe0638
Semyon Sporysh <beee.semok@gmail.com>
2020-12-03 16:22:25 +0300

    Make all functionality a part of one object

--- js/script.js
+++ js/script.js
@@ -1,72 +1,73 @@
 "use strict"
 
-let watchedFilmsCount;
-
-function start() {
-  watchedFilmsCount = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-  while (watchedFilmsCount == '' || watchedFilmsCount == null || isNaN(watchedFilmsCount)) {
-    watchedFilmsCount = +prompt('Сколько фильмов вы уже посмотрели?', '');
-  }
-}
-
-start();
-
 const personalMovieDb = {
-  count: watchedFilmsCount,
+  watchedFilmsCount: 0,
   movies: {},
   actors: {},
   genres: [],
-  privat: false
-}
+  private: false,
 
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const movie = prompt('Один из последних просмотренных фильмов?', ''),
-          rating = prompt('Один из последних просмотренных фильмов?', '');
+  start: function() {
+    this.watchedFilmsCount = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-    if (null != movie && null != rating && '' !== movie && '' !== rating && movie.length < 50 && rating.length < 50) {
-      personalMovieDb.movies[movie] = rating;
-    } else {
-      i--;
-      alert('Incorrect data. Result cant be empty string or string longer than 50 symbols. Please answer one more time.')
+    while (this.watchedFilmsCount == '' || this.watchedFilmsCount == null || isNaN(this.watchedFilmsCount)) {
+      this.watchedFilmsCount = +prompt('Сколько фильмов вы уже посмотрели?', '');
     }
+  },
+
+  rememberMyFilms: function() {
+    for (let i = 0; i < 2; i++) {
+      const movie = prompt('Один из последних просмотренных фильмов?', ''),
+        rating = prompt('Один из последних просмотренных фильмов?', '');
+
+      if (null != movie && null != rating && '' !== movie && '' !== rating && movie.length < 50 && rating.length < 50) {
+        this.movies[movie] = rating;
+      } else {
+        i--;
+        alert('Incorrect data. Result cant be empty string or string longer than 50 symbols. Please answer one more time.')
+      }
+    }
+  },
+
+  detectPersonalLevel: function() {
+    switch (true) {
+      case (this.watchedFilmsCount < 10):
+        console.log('Просмотрено довольно мало фильмов');
+        break;
+      case (this.watchedFilmsCount > 10 && this.watchedFilmsCount < 30):
+        console.log('Вы классический зритель');
+        break;
+      case (this.watchedFilmsCount > 30):
+        console.log('Вы киноман');
+        break;
+      default:
+        console.log('Произошла ошибка');
+        break;
+    }
+  },
+
+  showMyDB: function() {
+    if (!this.private) {
+      console.log(this);
+    }
+  },
+
+  writeYourGenres: function() {
+    for (let i = 1; i < 4; i++) {
+      this.genres[i - 1] = prompt(`Ваш любимый жанр №${i}`, '');
+
+      if (null === this.genres[i - 1] || '' === this.genres[i - 1]) {
+        alert('ВВЕДИТЕ КОРРЕКТНЫЕ ДАННЫЕ, ПОЖАЛУЙСТА!');
+        i--;
+      }
+    }
+
+    this.genres.forEach((item, i) => {
+      console.log(`Любимый жанр #${i + 1} - это ${item}`);
+    });
+  },
+
+  toggleVisibleMyDb: function () {
+    this.private = !this.private;
   }
 }
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-  switch (true) {
-    case (personalMovieDb.count < 10):
-      console.log('Просмотрено довольно мало фильмов');
-      break;
-    case (personalMovieDb.count > 10 && personalMovieDb.count < 30):
-      console.log('Вы классический зритель');
-      break;
-    case (personalMovieDb.count > 30):
-      console.log('Вы киноман');
-      break;
-    default:
-      console.log('Произошла ошибка');
-      break;
-  }
-}
-
-detectPersonalLevel();
-
-function showMyDB(isPrivate) {
-  if (!isPrivate) {
-    console.log(personalMovieDb);
-  }
-}
-
-showMyDB(personalMovieDb.privat);
-
-function writeYourGenres() {
-  for (let i = 1; i < 4; i++) {
-    personalMovieDb.genres[i - 1] = prompt(`Ваш любимый жанр №${i}`, '');
-  }
-}
-
-writeYourGenres();
